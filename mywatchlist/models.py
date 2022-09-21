f376fd2c4174e02a3b6f1058355de7180bc7656a
Emily Rumia Naomi <112367959+emilyrumia@users.noreply.github.com>
2022-09-21 11:06:58 +0700

    Update models.py

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class WatchList(models.Model):
     title_movie = models.TextField()
     release_date = models.CharField(max_length=800)
-    rating_movie = models.CharField(max_length=4)
+    rating_movie = models.CharField(max_length=100)
     review_movie = models.TextField()
     watched_status = models.CharField(max_length=200)
 
