8f1ae2898a9b6a50847b0ca34fb3d764a88e6ced
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2022-09-21 11:52:06 +0700

    Migrate Update

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class WatchList(models.Model):
-    title_movie = models.CharField(max_length=800)
-    release_date = models.CharField(max_length=800)
-    rating_movie = models.CharField(max_length=100)
+    title_movie = models.TextField()
+    release_date = models.TextField()
+    rating_movie = models.TextField()
     review_movie = models.TextField()
-    watched_status = models.CharField(max_length=200)
+    watched_status = models.TextField()
 
