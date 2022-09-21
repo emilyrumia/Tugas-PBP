e2fa1af7fe04dbee1eb68c7c8c3339c1e8b08dc7
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2022-09-22 00:43:08 +0700

    Coba lagi

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class WatchList(models.Model):
-    title_movie = models.TextField()
-    release_date = models.CharField(max_length=255)
-    rating_movie = models.CharField(max_length=255)
+    title_movie = models.CharField(max_length=300)
+    release_date = models.CharField(max_length=300)
+    rating_movie = models.CharField(max_length=300)
     review_movie = models.TextField()
-    watched_status = models.CharField(max_length=255)
+    watched_status = models.CharField(max_length=300)
 
