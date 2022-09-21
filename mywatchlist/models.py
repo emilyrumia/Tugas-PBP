9c5c5c934248bf2dfca45fe451076f4492c9ef91
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2022-09-22 00:38:17 +0700

    Mencoba Lagi

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class WatchList(models.Model):
-    title_movie = models.CharField(max_length=255)
+    title_movie = models.TextField()
     release_date = models.CharField(max_length=255)
     rating_movie = models.CharField(max_length=255)
     review_movie = models.TextField()
