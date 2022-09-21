ce208fc092facfa69f34ece08a14d5602466bcaf
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2022-09-21 16:03:03 +0700

    Tugas 3 Final

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,10 +1,13 @@
+from unittest.mock import DEFAULT
 from django.db import models
+
+DEFAULT = 0
 
 # Create your models here.
 class WatchList(models.Model):
     title_movie = models.CharField(max_length=255)
     release_date = models.CharField(max_length=255)
-    rating_movie = models.IntegerField()
+    rating_movie = models.CharField(max_length=255)
     review_movie = models.TextField()
     watched_status = models.CharField(max_length=255)
 
--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -4,18 +4,17 @@ from django.urls import resolve
 
 # Create your tests here.
 class test_watchlist(TestCase):
-    def test_html_url_exists(self):
-        response = self.client.get('/mywatchlist/html/')
-        self.assertEqual(response.status_code,200)
+    def test_html_url(self):
+        client = Client()
+        response = client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_xml_url_exists(self):
-        response = self.client.get('/mywatchlist/xml/')
-        self.assertEqual(response.status_code,200)
+    def test_xml_url(self):
+        client = Client()
+        response = client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_json_url_exists(self):
-        response = self.client.get('/mywatchlist/json/')
-        self.assertEqual(response.status_code,200)
-    
-    def test_mywatchlist_url_exists(self):
-        response = self.client.get('/mywatchlist/')
-        self.assertEqual(response.status_code,200)
+    def test_json_url(self):
+        client = Client()
+        response = client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
