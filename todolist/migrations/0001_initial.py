21a15a085e23acf87c1fedcadc14950d6665880b
Emily Rumia Naomi <112367959+emilyrumia@users.noreply.github.com>
2022-09-28 04:56:44 +0700

    Update 0001_initial.py

--- todolist/migrations/0001_initial.py
+++ todolist/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name="Task",
+            name="TaskList",
             fields=[
                 (
                     "id",
